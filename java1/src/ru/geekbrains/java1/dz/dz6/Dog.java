bea58df973eb9becd928756d1d44ac6fcefb5cf4
yajaneya <janeya@yandex.ru>
2018-08-05 09:29:59 +0300

    Произведена оптимизация ДЗ № 6
    Перенес все общие элементы в родительский класс.
    Класс Dog.
    (измененные классы Animals и Cat в предыдущих ветках)

--- java1/src/ru/geekbrains/java1/dz/dz6/Dog.java
+++ java1/src/ru/geekbrains/java1/dz/dz6/Dog.java
@@ -1,37 +1,27 @@
 package ru.geekbrains.java1.dz.dz6;
 
 public class Dog extends Animals{
-    private int distRun;
-    private double distJump;
-    private int distSwim;
-
 
     public Dog(String name) {
         super(name);
-        distRun = 500;
-        distJump = 0.5;
-        distSwim = 10;
+        super.distRun = 500;
+        super.distJump = 0.5;
+        super.distSwim = 10;
     }
 
     public Dog(String name, int distRun, double distJump, int distSwim) {
-        super(name);
-        this.distRun = distRun;
-        this.distJump = distJump;
-        this.distSwim = distSwim;
+        super(name, distRun, distJump, distSwim);
     }
 
     public void run (int dist) {
-        if ((dist<0)|(dist>distRun)) System.out.println("Пёс " + name + " не пробежит " + dist + " м.");
-        else System.out.println("Пёс " + name + " пробежал " + dist + " м.");
+        super.run (dist, "Пёс");
     }
 
     public void jump (double dist) {
-        if ((dist<0)|(dist>distJump)) System.out.println("Пёс " + name + " не допрыгнет " + dist + " м.");
-        else System.out.println("Пёс " + name + " прыгнул на " + dist + " м.");
+        super.jump (dist, "Пёс");
     }
 
     public void swim (int dist) {
-        if ((dist<0)|(dist>distSwim)) System.out.println("Пёс " + name + " не доплывёт " + dist + " м.");
-        else System.out.println("Пёс " + name + " проплыл таки " + dist + " м.");
+        super.swim (dist, "Пёс");
     }
 }
