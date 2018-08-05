043c3c90e40bc0f5c533e652563f1f7efe072dcd
yajaneya <janeya@yandex.ru>
2018-08-05 09:20:22 +0300

    Произведена оптимизация ДЗ № 6
    Перенес все общие элементы в родительский класс

--- java1/src/ru/geekbrains/java1/dz/dz6/Cat.java
+++ java1/src/ru/geekbrains/java1/dz/dz6/Cat.java
@@ -1,32 +1,27 @@
 package ru.geekbrains.java1.dz.dz6;
 
 public class Cat extends Animals{
-    private int distRun;
-    private double distJump;
 
     public Cat(String name) {
         super(name);
-        distRun = 200;
-        distJump = 2;
+        super.distRun = 200;
+        super.distJump = 2;
     }
 
     public Cat(String name, int distRun, double distJump) {
-        super(name);
-        this.distRun = distRun;
-        this.distJump = distJump;
-    }
+        super(name, distRun, distJump, 0);
+   }
 
     public void run (int dist) {
-        if ((dist<0)|(dist>distRun)) System.out.println("Кот " + name + " не пробежит " + dist + " м.");
-        else System.out.println("Кот " + name + " пробежал " + dist + " м.");
+        super.run (dist, "Кот");
     }
 
     public void jump (double dist) {
-        if ((dist<0)|(dist>distJump)) System.out.println("Кот " + name + " не допрыгнет " + dist + " м.");
-        else System.out.println("Кот " + name + " прыгнул на " + dist + " м.");
+        super.jump (dist, "Кот");
     }
 
     public void swim (int dist) {
-        System.out.println("Кот " + name + " не умеет плавать");
+        super.swim (dist, "Кот");
     }
+
 }
